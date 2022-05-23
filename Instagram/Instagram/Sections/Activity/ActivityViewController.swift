14d771ab73f9e28e5ec4641bb5b89686494cac91
Usr_Prime <>
2022-05-23 07:01:46 -0700

    Implementação do Activity ViewController e Model

---
+++ Instagram/Instagram/Sections/Activity/ActivityViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityViewController.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+    }
+
+}
---
+++ Instagram/Instagram/Sections/Activity/ActivityViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ActivityViewModel.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import Foundation
+
+class ActivityViewModel {
+    
+}
