f9384f5f0aec0b90604bb650adc7c389f8b6dd5a
Usr_Prime <>
2022-05-23 07:07:35 -0700

    Implementação do Perfil ViewController e Model

---
+++ Instagram/Instagram/Sections/Perfil/PerfilViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PerfilViewController.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import UIKit
+
+class PerfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+    }
+}
---
+++ Instagram/Instagram/Sections/Perfil/PerfilViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PerfilViewModel.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import Foundation
+
+class PerfilViewModel {
+    
+}
