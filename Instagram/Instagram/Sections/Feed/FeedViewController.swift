b28e88e1c4a8be928a8d09cc92ba9c2a1ab431c9
Usr_Prime <>
2022-05-23 06:57:20 -0700

    Implementação do Feed ViewController e Model

---
+++ Instagram/Instagram/Sections/Feed/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+    }
+}
---
+++ Instagram/Instagram/Sections/Feed/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import Foundation
+
+class FeedViewModel {
+    
+}
