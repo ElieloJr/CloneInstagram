e7b334b3966d2be167aa45b63bf3d5698f68b64b
Usr_Prime <>
2022-05-23 07:05:01 -0700

    Implementação do ToMeet ViewController e Model

---
+++ Instagram/Instagram/Sections/ToMeet/ToMeetViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ToMeetViewController.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import UIKit
+
+class ToMeetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+    }
+
+}
---
+++ Instagram/Instagram/Sections/ToMeet/ToMeetViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ToMeetViewModel.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 23/05/22.
+//
+
+import Foundation
+
+class ToMeetViewModel {
+    
+}
