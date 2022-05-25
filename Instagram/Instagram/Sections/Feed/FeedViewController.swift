28af46bc86de9145d0d924496f37f48e5e17c2c7
Usr_Prime <>
2022-05-25 05:51:44 -0700

    Implementação da Cell para Posts

--- Instagram/Instagram/Sections/Feed/FeedViewController.swift
+++ Instagram/Instagram/Sections/Feed/FeedViewController.swift
@@ -13,6 +13,8 @@ class FeedViewController: UIViewController {
         let tableView = UITableView()
         let headerTable = StatusHeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.width/4))
         tableView.tableHeaderView = headerTable
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier)
+        tableView.showsVerticalScrollIndicator = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
@@ -42,16 +44,18 @@ class FeedViewController: UIViewController {
 }
 
 extension FeedViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.height/1.8
+    }
 }
 
 extension FeedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
         return cell
     }
     
