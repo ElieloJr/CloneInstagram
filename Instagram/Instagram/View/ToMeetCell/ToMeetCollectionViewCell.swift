7e3b35d152ac8ec54c6d5508084f317f0d71d29f
Usr_Prime <>
2022-05-25 14:09:31 -0700

    Implementação da Célula da tela do toMeet

--- Instagram/Instagram/View/ToMeetCell/ToMeetCollectionViewCell.swift
+++ Instagram/Instagram/View/ToMeetCell/ToMeetCollectionViewCell.swift
@@ -11,12 +11,25 @@ class ToMeetCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "ToMeetCollectionViewCell"
     
+    private lazy var smallImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .darkGray
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .darkGray
+        smallImageView.frame = contentView.bounds
+        
+        addSubview(smallImageView)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(with post: UIImage) {
+        smallImageView.image = post
     }
 }
