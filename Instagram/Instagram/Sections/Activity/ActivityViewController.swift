306b17e915ede20d084e1c3cb99707afbc1f636d
Usr_Prime <>
2022-05-25 06:22:19 -0700

    Implementação da TableView de Activity

--- Instagram/Instagram/Sections/Activity/ActivityViewController.swift
+++ Instagram/Instagram/Sections/Activity/ActivityViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class ActivityViewController: UIViewController {
+    
+    private lazy var activityTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+    }
+
+    private func setupView() {
         view.backgroundColor = Colors().defautBackground
         
         navigationItem.title = Titles().activityTitle
         self.navigationController?.navigationBar.titleTextAttributes = Titles().defaultFont
+        
+        activityTableView.delegate = self
+        activityTableView.dataSource = self
+        
+        view.addSubview(activityTableView)
+        activityTableView.frame = view.bounds
     }
+}
 
+extension ActivityViewController: UITableViewDelegate {
+    
+}
+
+extension ActivityViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
