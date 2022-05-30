0eb739e6977bf0682c4ee01523517a3b32e82814
Usr_Prime <>
2022-05-30 05:14:16 -0700

    Criação da viewController e da ViewModel

---
+++ Instagram/Instagram/Sections/Results/ResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsViewController.swift
+//  Instagram
+//
+//  Created by Usr_Prime on 30/05/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
