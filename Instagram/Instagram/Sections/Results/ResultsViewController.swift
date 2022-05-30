25b3d0bcd161705650524886a15565be76994702
Usr_Prime <>
2022-05-30 05:37:08 -0700

    Implementação de TableView para os resultados

--- Instagram/Instagram/Sections/Results/ResultsViewController.swift
+++ Instagram/Instagram/Sections/Results/ResultsViewController.swift
@@ -11,15 +11,48 @@ class ResultsViewController: UIViewController {
     
     private lazy var searchController: UISearchController = {
         let searchController = UISearchController()
-        searchController.searchBar.placeholder = "Pesquisar usuário"
         searchController.searchBar.searchBarStyle = .minimal
         return searchController
+    }()
+    
+    private lazy var resultsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.showsVerticalScrollIndicator = false
+        return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = Colors().defautBackground
         
+        setupView()
+        setupConstraints()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = Colors().defautBackground
         navigationItem.searchController = searchController
+        
+        resultsTableView.delegate = self
+        resultsTableView.dataSource = self
+        
+        view.addSubview(resultsTableView)
+    }
+    
+    private func setupConstraints() {
+        resultsTableView.frame = view.bounds
+    }
+}
+
+extension ResultsViewController: UITableViewDelegate {
+    
+}
+
+extension ResultsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
