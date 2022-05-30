158667b056c0a1d4cd8c46f3c90db275d9bec3ff
Usr_Prime <>
2022-05-30 05:24:21 -0700

    Implementação da TabBar que leva para a página de Resultados

--- Instagram/Instagram/Sections/Results/ResultsViewController.swift
+++ Instagram/Instagram/Sections/Results/ResultsViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "Pesquisar usuário"
+        searchController.searchBar.searchBarStyle = .minimal
+        return searchController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = Colors().defautBackground
         
+        navigationItem.searchController = searchController
     }
 }
