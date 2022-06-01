37485a7aebea4e7124e39fda788a52e8bbb3db5c
Usr_Prime <>
2022-06-01 07:36:40 -0700

    Implementação da foto de Perfil do User

--- Instagram/Instagram/Sections/Post/PostViewController.swift
+++ Instagram/Instagram/Sections/Post/PostViewController.swift
@@ -25,11 +25,41 @@ class PostViewController: UIViewController {
         
         return leftButton
     }()
+    
+    private lazy var pictureUserImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .gray
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 18
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private lazy var pictureUserView: UIView = {
+        let view = UIView()
+        view.layer.borderColor = UIColor.red.cgColor
+        view.layer.borderWidth = 3
+        view.layer.cornerRadius = 25
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(pictureUserImageView)
+        
+        pictureUserImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 7).isActive = true
+        pictureUserImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 7).isActive = true
+        pictureUserImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -7).isActive = true
+        pictureUserImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -7).isActive = true
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+        setupConstraints()
     }
     
     private func setupView() {
@@ -37,6 +67,15 @@ class PostViewController: UIViewController {
         
         self.navigationController?.navigationBar.titleTextAttributes = Titles().feedFont
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+        
+        view.addSubview(pictureUserView)
+    }
+    
+    private func setupConstraints() {
+        pictureUserView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        pictureUserView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        pictureUserView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pictureUserView.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
     @objc func backButton() {
